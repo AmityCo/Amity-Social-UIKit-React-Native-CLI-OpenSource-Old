75aa6a33d51b02450c9e95b97c975f3a5e47332e
topAmity <112688936+topAmity@users.noreply.github.com>
2023-09-12 21:40:14 +0700

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# amity-react-native-social-ui-kit
+
+Social UIKit
+
+## Installation
+
+```sh
+npm install amity-react-native-social-ui-kit
+```
+
+## Usage
+
+```js
+import { multiply } from 'amity-react-native-social-ui-kit';
+
+// ...
+
+const result = await multiply(3, 7);
+```
+
+## Contributing
+
+See the [contributing guide](CONTRIBUTING.md) to learn how to contribute to the repository and the development workflow.
+
+## License
+
+MIT
+
+---
+
+Made with [create-react-native-library](https://github.com/callstack/react-native-builder-bob)
---
+++ src/index.tsx
@@ -0,0 +1,22 @@
+import { NativeModules, Platform } from 'react-native';
+
+const LINKING_ERROR =
+  `The package 'amity-react-native-social-ui-kit' doesn't seem to be linked. Make sure: \n\n` +
+  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
+  '- You rebuilt the app after installing the package\n' +
+  '- You are not using Expo Go\n';
+
+const AmityReactNativeSocialUiKit = NativeModules.AmityReactNativeSocialUiKit
+  ? NativeModules.AmityReactNativeSocialUiKit
+  : new Proxy(
+      {},
+      {
+        get() {
+          throw new Error(LINKING_ERROR);
+        },
+      }
+    );
+
+export function multiply(a: number, b: number): Promise<number> {
+  return AmityReactNativeSocialUiKit.multiply(a, b);
+}
